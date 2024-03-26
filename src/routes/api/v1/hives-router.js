aff1a6d122dc1bb09f2b8a83003590376263782d
Maria Fredriksson <mf223wk@student.lnu.se>
2024-03-26 11:42:50 +0100

    Delete some old code in routes

--- src/routes/api/v1/hives-router.js
+++ src/routes/api/v1/hives-router.js
@@ -2,11 +2,10 @@
  * API version 1 routes.
  *
  * @author Maria Fredriksson
- * @version 2.0.0
+ * @version 1.0.0
  */
 
 import express from 'express'
-// import { router as resourcesRouter } from './resources-router.js'
 import { HivesController } from './../../../controllers/api/hives-controller.js'
 import { HiveStatusController } from './../../../controllers/api/hive-status-controller.js'
 
@@ -14,9 +13,6 @@ export const router = express.Router()
 
 const hivesController = new HivesController()
 const statusController = new HiveStatusController()
-
-// GET tasks
-// router.get('/', (req, res, next) => controller.getAll(req, res, next))
 
 // POST hives
 router.post('/', (req, res, next) => hivesController.addHive(req, res, next))
--- src/routes/api/v1/router.js
+++ src/routes/api/v1/router.js
@@ -2,11 +2,10 @@
  * API version 1 routes.
  *
  * @author Maria Fredriksson
- * @version 2.0.0
+ * @version 1.0.0
  */
 
 import express from 'express'
-// import { router as resourcesRouter } from './resources-router.js'
 import { router as hivesRouter } from './hives-router.js'
 import { router as harvestRouter } from './harvest-router.js'
 
@@ -15,4 +14,3 @@ export const router = express.Router()
 router.get('/', (req, res) => res.json({ message: 'Hooray! Welcome to the Beehive Monitoring API!' }))
 router.use('/hives', hivesRouter)
 router.use('/harvest', harvestRouter)
-// router.use('/hives', resourcesRouter)
