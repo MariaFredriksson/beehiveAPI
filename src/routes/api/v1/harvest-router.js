334d2d34f5dd930d82b4233511ce77cf792760ad
Maria Fredriksson <mf223wk@student.lnu.se>
2024-03-26 17:03:14 +0100

    Delete some old code

--- src/routes/api/v1/harvest-router.js
+++ src/routes/api/v1/harvest-router.js
@@ -6,32 +6,11 @@
  */
 
 import express from 'express'
-// import { router as resourcesRouter } from './resources-router.js'
-// import { HivesController } from './../../../controllers/api/hives-controller.js'
 import { HarvestController } from './../../../controllers/api/harvest-controller.js'
 
 export const router = express.Router()
 
-// const controller = new HivesController()
 const controller = new HarvestController()
 
 // POST harvest
 router.post('/', (req, res, next) => controller.addHarvest(req, res, next))
-
-// GET tasks
-// router.get('/', (req, res, next) => controller.getAll(req, res, next))
-
-// // GET hives/:id
-// router.get('/:id', (req, res, next) => controller.getHiveStatus(req, res, next))
-
-// // GET hives/:id/flow
-// router.get('/:id/flow', (req, res, next) => controller.getRecentFlow(req, res, next))
-
-// // GET hives/:id/humidity
-// router.get('/:id/humidity', (req, res, next) => controller.getRecentHumidity(req, res, next))
-
-// // GET hives/:id/temperature
-// router.get('/:id/temperature', (req, res, next) => controller.getRecentTemperature(req, res, next))
-
-// // GET hives/:id/weight
-// router.get('/:id/weight', (req, res, next) => controller.getRecentWeight(req, res, next))
