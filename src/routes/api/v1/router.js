3c74b842945454e4680d77f4adf583ff7d13c107
Maria Fredriksson <mf223wk@student.lnu.se>
2024-03-27 11:48:59 +0100

    Add working authentication before most requests

--- src/routes/api/v1/router.js
+++ src/routes/api/v1/router.js
@@ -11,6 +11,54 @@ import { router as harvestRouter } from './harvest-router.js'
 import { router as mobileBeehiveRouter } from './mobile-beehive-router.js'
 import { router as userRouter } from './user-router.js'
 import { createLink } from './../../../utils/linkUtils.js'
+import createHttpError from 'http-errors'
+import jwt from 'jsonwebtoken'
+
+/**
+ * Authenticates requests. Controls that the JWT is a correct JWT.
+ *
+ * If authentication is successful, `req.user`is populated and the
+ * request is authorized to continue.
+ * If authentication fails, an unauthorized response will be sent.
+ *
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ */
+const authenticateJWT = (req, res, next) => {
+  try {
+    // The ? is there because the header authorization might not exist, and if it isn't there the split will not throw an exception now, but be caught in the next if
+    const [authenticationScheme, token] = req.headers.authorization?.split(' ')
+
+    // Check if the authentication scheme is the right type, since we want the JWT to be sent as a bearer
+    if (authenticationScheme !== 'Bearer') {
+      throw new Error('Invalid authentication scheme.')
+    }
+
+    // Authenticate the user with the built in method verify in jsonwebtoken, which will throw an exception if the token is invalid
+    const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, { algorithms: ['HS256'] })
+
+    console.log(`User ${payload.email} authenticated.`)
+
+    // Log when the token was issued and when it expires
+    console.log('The token was issued at:', new Date(payload.iat * 1000))
+    console.log('The token expires at:', new Date(payload.exp * 1000))
+
+    // Populate the req.user with useful information from the payload
+    req.user = {
+      email: payload.email,
+      firstName: payload.given_name,
+      lastName: payload.family_name,
+      role: payload.x_role
+    }
+
+    next()
+  } catch (err) {
+    const error = createHttpError(401)
+    error.cause = err
+    next(error)
+  }
+}
 
 export const router = express.Router()
 
@@ -30,7 +78,10 @@ router.get('/', (req, res) => {
   })
 })
 
-router.use('/hives', hivesRouter)
-router.use('/harvest', harvestRouter)
-router.use('/mobile-beehive-request', mobileBeehiveRouter)
+// User routes (doesn't require JWT authentication for all actions)
 router.use('/user', userRouter)
+
+// Apply authenticateJWT middleware before accessing the specific routes
+router.use('/hives', authenticateJWT, hivesRouter)
+router.use('/harvest', authenticateJWT, harvestRouter)
+router.use('/mobile-beehive-request', authenticateJWT, mobileBeehiveRouter)
